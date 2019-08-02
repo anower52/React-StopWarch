c885d789166f7ffa0e4c078a3a08e7857bc07079
anower00 <anower.hasan@gmail.com>
2019-08-03 02:46:19 +0600

    stop watch design with button working

---
+++ src/Components/App.js
@@ -0,0 +1,40 @@
+import React, { Component} from 'react'
+import './App.css';
+import Title from './Title/Title'
+import CountDown from './CountDown/CountDown'
+import Controller from './Controller/Controller'
+
+class App extends Component{
+
+  constructor(props){
+    super(props)
+
+    this.state = {
+      time: {
+        min : 0,
+        sec:0,
+        mili:0
+      }
+    }
+  }
+
+
+render(){
+
+  return (
+    <div className="App">
+      <div className="container py-5">
+         <div className="row">
+           <div className="col-sm-8 offset-sm-2">
+              <Title />
+              <CountDown time = {this.state.time} />
+              <Controller />
+           </div>
+         </div>
+      </div>
+    </div>
+  );
+}
+}
+
+export default App;
